ef225c7a5616b6115d83e41a529d309e15da53c8
ivshxvi <ivhsxvi@gmail.com>
2024-03-18 09:51:18 +0000

    Add the session model

---
+++ backend/model/session.js
@@ -0,0 +1,39 @@
+const { v4: uuid } = require("uuid")
+const db = require("../db/db")
+
+const createSession = async (user_id) => {
+
+    const response = await db.query(
+        `INSERT INTO auth_session (user_id, session_id) VALUES ($1, $2) RETURNING *`,
+        [user_id, uuid(undefined, undefined, undefined)]
+    )
+    return response.rows[0]
+}
+
+const getSession = async (token) => {
+
+    const response = await db.query(
+        `SELECT * FROM auth_session WHERE token = $1`,
+        [token]
+    )
+    try{
+        return response.rows[0]
+    } catch {
+        throw new Error ("Token not found")
+    }
+}
+
+const destroySession = async (token) => {
+
+    return await db.query(
+        `DELETE FROM auth_session WHERE token = $1`,
+        [token]
+    )
+}
+
+
+module.exports = {
+    createSession,
+    getSession,
+    destroySession
+}
