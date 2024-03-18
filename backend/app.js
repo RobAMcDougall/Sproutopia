49669e38a912e4f2f7826fdfe5bbadaba5361e2a
ivshxvi <ivhsxvi@gmail.com>
2024-03-18 09:24:30 +0000

    Add accounts routes

--- backend/app.js
+++ backend/app.js
@@ -5,4 +5,16 @@ const cors = require("@koa/cors");
 const {bodyParser} = require("@koa/bodyparser");
 app.use(cors()).use(bodyParser());
 
+const Router = require('@koa/router')
+const controllers = require('./controller')
+
+const accounts = new Router({prefix: "/account"})
+acoounts.post("/register", controllers.accounts.register)
+accounts.post("/login", controllers.accounts.login)
+accounts.delete("/logout", controllers.account.logout)
+app.use(accounts.routes()).use(accounts.allowedMethods())
+// Can you do: app.use(accounts.routes(), accounts.allowedMethods())
+
+
+
 module.exports = app;
