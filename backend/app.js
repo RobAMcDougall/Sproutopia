8e7c435c3b677a768f6a0a0bd2c2081c9c4951e4
90 <hi@90.gripe>
2024-03-18 12:44:18 +0000

    Fix account controller references

--- backend/app.js
+++ backend/app.js
@@ -9,8 +9,8 @@ const Router = require('@koa/router')
 const controllers = require('./controller')
 
 const accounts = new Router({prefix: "/account"})
-accounts.post("/register", controllers.accounts.register)
-accounts.post("/login", controllers.accounts.login)
+accounts.post("/register", controllers.account.register)
+accounts.post("/login", controllers.account.login)
 accounts.delete("/logout", controllers.account.logout)
 app.use(accounts.routes()).use(accounts.allowedMethods())
 
