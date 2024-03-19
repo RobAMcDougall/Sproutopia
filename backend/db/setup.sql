1a25a45659914b625f7b3aa30ce8394097214020
ivshxvi <ivhsxvi@gmail.com>
2024-03-19 10:01:49 +0000

    Add dummy data to setup.sql

--- backend/db/setup.sql
+++ backend/db/setup.sql
@@ -21,8 +21,8 @@ CREATE TABLE "all_plants"(
     "name" VARCHAR(24) NOT NULL,
     "latin_name" VARCHAR(48) NOT NULL,
     "description" TEXT NOT NULL,
-    "season" CHAR(6) NOT NULL,
-    "soil" VARCHAR(24) NOT NULL,
+    "season" VARCHAR(12) NOT NULL,
+    "soil" VARCHAR(50) NOT NULL,
     "watering_freq" INTEGER NOT NULL,
     "harvesting" TEXT NOT NULL,
     "sow_indoors_start" DATE NOT NULL,
@@ -45,3 +45,20 @@ CREATE TABLE "planted_veg"(
     "growth_stage" INTEGER NOT NULL,
     "date_planted" DATE NOT NULL
 );
+
+INSERT INTO "user" ("username", "password", "email")
+VALUES 
+    ('john_doe', 'hashed_password_123', 'john@example.com'),
+    ('jane_smith', 'hashed_password_456', 'jane@example.com'),
+    ('bob_jackson', 'hashed_password_789', 'bob@example.com');
+
+INSERT INTO all_plants ("name", "latin_name", "description", "season", "soil", "watering_freq", "harvesting", "sow_indoors_start", "sow_indoors_end", "sow_outdoors_start", "sow_outdoors_end", "harvest_start", "harvest_end", "germination_min", "germination_max", "storage", "sow_and_plant_info", "image_url")
+VALUES 
+    ('Tomato', 'Solanum lycopersicum', 'Tomatoes are the edible berry of the plant Solanum lycopersicum, commonly known as a tomato plant.', 'Summer', 'Well-drained, fertile soil', 2, 'Harvest when the fruits are ripe.', '2024-03-01', '2024-04-15', '2024-04-15', '2024-05-15', '2024-06-15', '2024-09-15', 7, 14, 'Store in a cool, dry place.', 'Sow indoors in pots or trays.', 'https://example.com/tomato.jpg'),
+    ('Lettuce', 'Lactuca sativa', 'Lettuce is a leafy vegetable that is often used in salads.', 'Spring, Fall', 'Well-drained, loamy soil', 1, 'Harvest the outer leaves as needed.', '2024-02-15', '2024-03-15', '2024-03-15', '2024-04-15', '2024-04-15', '2024-05-30', 7, 10, 'Store in the refrigerator.', 'Sow directly into the garden bed.', 'https://example.com/lettuce.jpg');
+
+
+INSERT INTO planted_veg ("user", "plant_id", "growth_stage", "date_planted")
+VALUES
+    (1, 1, 2, '2024-03-05'),
+    (2, 2, 1, '2024-03-10');
