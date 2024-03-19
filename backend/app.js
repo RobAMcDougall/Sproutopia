0c698f76bd954d42f581fefce6d221f17edc1443
Rob McDougall <Rob7McDougall@hotmail.comgit config --global user.name RobAMcDougallqssh-keygenuitquit>
2024-03-19 16:34:43 +0000

    merged

---
+++ backend/app.js
@@ -0,0 +1,28 @@
+const Koa = require("koa");
+const app = new Koa();
+
+const cors = require("@koa/cors");
+const {bodyParser} = require("@koa/bodyparser");
+app.use(cors()).use(bodyParser());
+
+const Router = require("@koa/router");
+const controllers = require("./controller");
+
+const accounts = new Router({prefix: "/account"});
+accounts.post("/register", controllers.account.register);
+accounts.post("/login", controllers.account.login);
+accounts.delete("/logout", controllers.account.logout);
+accounts.get("/", controllers.account.getFromSession);
+app.use(accounts.routes()).use(accounts.allowedMethods());
+
+const plants = new Router({prefix: "/plants"});
+plants.get("/user/:user", controllers.plant.getPlantsByUser);
+plants.get("/user/:user/details", controllers.plant.getPlantsDetailsByUser);
+plants.post("/user/:user/:plant", controllers.plant.addPlantForUser);
+plants.patch("/user/:plant", controllers.plant.incrementGrowthStage);
+plants.delete("/user/:plant", controllers.plant.deletePlant);
+plants.get("/all", controllers.plant.getAllPlants);
+plants.get("/:id", controllers.plant.getPlantById);
+app.use(plants.routes()).use(plants.allowedMethods());
+
+module.exports = app;
---
+++ backend/db/setup.sql
@@ -0,0 +1,64 @@
+DROP TABLE IF EXISTS "planted_veg";
+DROP TABLE IF EXISTS "all_plants";
+DROP TABLE IF EXISTS "auth_session";
+DROP TABLE IF EXISTS "user";
+
+CREATE TABLE "user"(
+    "id" INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    "username" VARCHAR(24) NOT NULL,
+    "password" CHAR(60) NOT NULL,
+    "email" VARCHAR(255) NOT NULL,
+    "preferences" JSON NULL
+);
+
+CREATE TABLE "auth_session"(
+    "token" CHAR(36) PRIMARY KEY UNIQUE NOT NULL,
+    "user" INTEGER NOT NULL REFERENCES "user"
+);
+
+CREATE TABLE "all_plants"(
+    "id" INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    "name" VARCHAR(24) NOT NULL,
+    "latin_name" VARCHAR(48) NOT NULL,
+    "description" TEXT NOT NULL,
+    "season" VARCHAR(12) NOT NULL,
+    "soil" VARCHAR(50) NOT NULL,
+    "watering_freq" INTEGER NOT NULL,
+    "harvesting" TEXT NOT NULL,
+    "sow_indoors_start" DATE NOT NULL,
+    "sow_indoors_end" DATE NOT NULL,
+    "sow_outdoors_start" DATE NOT NULL,
+    "sow_outdoors_end" DATE NOT NULL,
+    "harvest_start" DATE NOT NULL,
+    "harvest_end" DATE NOT NULL,
+    "germination_min" INTEGER NOT NULL,
+    "germination_max" INTEGER NOT NULL,
+    "storage" TEXT NOT NULL,
+    "sow_and_plant_info" TEXT NOT NULL,
+    "image_url" TEXT NOT NULL
+);
+
+CREATE TABLE "planted_veg"(
+    "id" INTEGER NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    "user" INTEGER NOT NULL REFERENCES "user",
+    "plant_id" INTEGER NOT NULL REFERENCES all_plants,
+    "growth_stage" INTEGER NOT NULL,
+    "date_planted" DATE NOT NULL
+);
+
+INSERT INTO "user" ("username", "password", "email")
+VALUES 
+    ('john_doe', 'hashed_password_123', 'john@example.com'),
+    ('jane_smith', 'hashed_password_456', 'jane@example.com'),
+    ('bob_jackson', 'hashed_password_789', 'bob@example.com');
+
+INSERT INTO all_plants ("name", "latin_name", "description", "season", "soil", "watering_freq", "harvesting", "sow_indoors_start", "sow_indoors_end", "sow_outdoors_start", "sow_outdoors_end", "harvest_start", "harvest_end", "germination_min", "germination_max", "storage", "sow_and_plant_info", "image_url")
+VALUES 
+    ('Tomato', 'Solanum lycopersicum', 'Tomatoes are the edible berry of the plant Solanum lycopersicum, commonly known as a tomato plant.', 'Summer', 'Well-drained, fertile soil', 2, 'Harvest when the fruits are ripe.', '2024-03-01', '2024-04-15', '2024-04-15', '2024-05-15', '2024-06-15', '2024-09-15', 7, 14, 'Store in a cool, dry place.', 'Sow indoors in pots or trays.', 'https://example.com/tomato.jpg'),
+    ('Lettuce', 'Lactuca sativa', 'Lettuce is a leafy vegetable that is often used in salads.', 'Spring, Fall', 'Well-drained, loamy soil', 1, 'Harvest the outer leaves as needed.', '2024-02-15', '2024-03-15', '2024-03-15', '2024-04-15', '2024-04-15', '2024-05-30', 7, 10, 'Store in the refrigerator.', 'Sow directly into the garden bed.', 'https://example.com/lettuce.jpg');
+
+
+INSERT INTO planted_veg ("user", "plant_id", "growth_stage", "date_planted")
+VALUES
+    (1, 1, 2, '2024-03-05'),
+    (2, 2, 1, '2024-03-10');
