69e2624df08c4bffe6404c24742fab127acdadfe
lottie-isobel <charlotteeaston@hortmail.co.uk>
2024-03-19 15:55:51 +0000

    style registration page

--- src/pages/LoginRegisterPage/RegisterPage.jsx
+++ src/pages/LoginRegisterPage/RegisterPage.jsx
@@ -1,7 +1,54 @@
-import React from 'react'
+import React from "react";
 
 export default function RegisterPage() {
   return (
-    <div>RegisterPage</div>
-  )
+    <div className="login-register-page">
+      <img
+        className="logo-header"
+        src="src/assets/logo-header.png"
+        alt="Sproutopia logo"
+      />
+      <img
+        className="bumblebee bumblebee1"
+        src="src/assets/bumblebee.png"
+        alt="bumblebee"
+      />
+      <h1>New User?</h1>
+      <h2>Sign up for an account below</h2>
+      <form className="login-register register">
+        <p className="form-label">Name</p>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          placeholder="Enter your name"
+          required
+        />
+        <p className="form-label">Email address</p>
+        <input
+          type="text"
+          id="email"
+          name="email"
+          placeholder="Enter your email address"
+          required
+        />
+        <p className="form-label">Password</p>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          placeholder="Enter your password"
+          required
+        />
+        <div className="button-wrap">
+          <input className="signup-button" type="submit" value="Signup" />
+        </div>
+      </form>
+      <img
+        className="bumblebee bumblebee2"
+        src="src/assets/bumblebee2.png"
+        alt="bumblebee"
+      />
+    </div>
+  );
 }
