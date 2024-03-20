e76f06c9d08e3d54468e9523c9fc457915783101
Rob McDougall <Rob7McDougall@hotmail.comgit config --global user.name RobAMcDougallqssh-keygenuitquit>
2024-03-20 12:41:58 +0000

    add unstyled navbar

---
+++ src/components/Header/Navbar.jsx
@@ -0,0 +1,39 @@
+import { NavLink, Outlet } from "react-router-dom";
+// import { useAuth } from "";
+import "./navbar.css";
+
+function Navbar() {
+  // const { logout } = useAuth;
+  // const handleLogout = () => {
+  //   logout();
+  //   navigate("/");
+  // };
+  return (
+    <>
+      <div className="header">
+        <div className="logo-container">
+          <NavLink to="/">
+            <img className="logo" src="" />
+          </NavLink>
+        </div>
+        <nav className="navLink">
+          <NavLink className="link" to="/garden">
+            Garden
+          </NavLink>
+          <NavLink className="link" to="/calendar">
+            Calendar
+          </NavLink>
+          <NavLink className="link" to="/kitchen">
+            Kitchen
+          </NavLink>
+          <NavLink className="link" to="/" onClick={handleLogout}>
+            LogOut
+          </NavLink>
+        </nav>
+      </div>
+      <Outlet />
+    </>
+  );
+}
+
+export default Navbar;
