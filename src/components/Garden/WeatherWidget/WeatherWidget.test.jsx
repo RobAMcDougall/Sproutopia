eea73e56474dcfa3e1710d55fb8150a685595f2d
ivshxvi <ivhsxvi@gmail.com>
2024-03-26 10:56:04 +0000

    Add WeatherWidget tests

---
+++ src/components/Garden/WeatherWidget/WeatherWidget.test.jsx
@@ -0,0 +1,22 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+import WeatherWidget from './WeatherWidget';
+
+describe('WeatherWidget', () => {
+  it('renders correctly', () => {
+    render(<WeatherWidget />);
+  });
+
+  it('matches snapshot', () => {
+    const { asFragment } = render(<WeatherWidget />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it('updates postcode state when typing in input field', () => {
+    render(<WeatherWidget />);
+    const input = screen.getByLabelText('Enter your postcode:');
+    fireEvent.change(input, { target: { value: '12345' } });
+    expect(input.value).toBe('12345');
+  });
+})
